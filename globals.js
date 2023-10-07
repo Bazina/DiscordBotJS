0b93afd64ec80cecc027a998597b93afe9aa1921
bazina <youssefbazina7@gmail.com>
2023-10-07 13:39:00 +0300

    init new commands.

--- globals.js
+++ globals.js
@@ -17,6 +17,7 @@ global.client = new Client({
 
 client.once('ready', () => {
     global.GUILD_ID = client.guilds.cache.first().id;
+    require('./initialization.js');
 });
 
 client.on('guildCreate', (guild) => {
