0114c426973df4e283b388d7b1989ba09fd9f868
bazina <youssefbazina7@gmail.com>
2023-10-07 20:46:55 +0300

    decrease time interval to 1 min

--- helper_functions.js
+++ helper_functions.js
@@ -206,7 +206,7 @@ setInterval(() => {
         let changes = await pullChanges(driveClient, DRIVE_ID, lastTimestamp.toISOString());
         await loopOverChanges(changes);
     });
-}, 1800000);
+}, 60000);
 
 module.exports = {
     replyWithCourseData,
