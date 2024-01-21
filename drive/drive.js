d19af36e0d38e04c6a468bb48356623170deee39
bazina <youssefbazina7@gmail.com>
2024-01-22 01:02:41 +0200

    Repackage files

--- drive/drive.js
+++ drive/drive.js
@@ -6,8 +6,8 @@ const {google} = require('googleapis');
 
 const SCOPES = ['https://www.googleapis.com/auth/drive.readonly', 'https://www.googleapis.com/auth/drive.activity.readonly'];
 
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const TOKEN_PATH = path.join(process.cwd(), '/etc/secrets/token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), '/etc/secrets/credentials.json');
 
 /**
  * Reads previously authorized credentials from the save file.
